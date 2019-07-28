ecf2d3b5511eaf7328758a1be0ed7380eda23652
MGYOSBEL <mgyosbel@gmail.com>
2019-07-28 02:27:29 -0400

    first-milestone-ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found-component';
+
+const appRoutes: Routes = [
+  {path: 'crisis-center' , component: CrisisListComponent},
+  {path: 'heroes' , component: HeroesListComponent},
+  { path: '',   redirectTo: '/heroes', pathMatch: 'full' },
+  {path: '**' , component: PageNotFoundComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrisisListComponent,
+    HeroesListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    RouterModule.forRoot(appRoutes, {enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
